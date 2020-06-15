4ce2a96c794e821840882f3568e4cdfd3b47fa98
yulahabogoslavets <speed0435@gmail.com>
2020-06-15 11:47:22 +0200

    Updates

---
+++ precache-manifest.940529c29607a4d6c4e0719803a6506b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "113adcec83d25bff2925c137f2796854",
+    "url": "/app-planer/index.html"
+  },
+  {
+    "revision": "1a4c00548771ca3d3daf",
+    "url": "/app-planer/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b04dc7d67fe70c2e5552",
+    "url": "/app-planer/static/css/main.7593aca8.chunk.css"
+  },
+  {
+    "revision": "1a4c00548771ca3d3daf",
+    "url": "/app-planer/static/js/2.fe09d5e4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/app-planer/static/js/2.fe09d5e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b04dc7d67fe70c2e5552",
+    "url": "/app-planer/static/js/main.faa18999.chunk.js"
+  },
+  {
+    "revision": "0399cea42f9fdbe86bb8",
+    "url": "/app-planer/static/js/runtime-main.2ed9531d.js"
+  }
+]);
